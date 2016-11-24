1f95155a27cd242324d14f46e9c9ac1a13142d3e
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-11-25 00:18:01 +0200

    tessting codeship build pipeline

--- app/components/modules/Header.jsx
+++ app/components/modules/Header.jsx
@@ -165,6 +165,7 @@ class Header extends React.Component {
                                     </Link>
                                 </li>
 
+
                                 <li className="Header__top-steemit show-for-medium noPrint">
                                     <Link to={logo_link}>{APP_NAME}<span className="beta">alpha</span></Link>
                                 </li>
@@ -172,6 +173,7 @@ class Header extends React.Component {
                                 {(topic_link || user_name || page_name) && <li className="delim show-for-medium">|</li>}
                                 {topic_link && <li className="Header__top-topic">{topic_link}</li>}
                                 {user_name && <li><Link to={`/@${user_name}`}>{user_name}</Link></li>}
+                                <li><span>Welcome golos developers</span><p>commits to _dev branch now expected to be autodeployed to dev.golos.io</p></li>
                                 {page_name && <li><span>{page_name}</span></li>}
                                 {(topic_link || user_name || page_name) && sort_order && <li className="delim show-for-small-only">|</li>}
                                 {selected_sort_order && <DropdownMenu className="Header__sort-order-menu show-for-small-only" items={sort_order_menu} selected={selected_sort_order[1]} el="li" />}

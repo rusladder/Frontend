f4b2b992aeb5b8c0a63eacaa79bb215f7533ba8d
b1acksun <tkachenko.igor88@gmail.com>
2017-07-21 19:24:30 +0300

    added password required for advanced options

--- app/components/elements/WalletSubMenu.jsx
+++ app/components/elements/WalletSubMenu.jsx
@@ -1,36 +1,68 @@
-import React from 'react';
+import React, { PropTypes } from 'react'
 import { Link } from 'react-router';
+import { connect } from 'react-redux'
+import user from 'app/redux/User'
 import NotifiCounter from 'app/components/elements/NotifiCounter';
 import tt from 'counterpart';
 
-export default ({account_name}) => {
-    return <div className="row">
-        <div className="columns small-10 medium-12 medium-expand left-column">
-            <ul className="WalletSubMenu menu">
-                <li>
-                    <Link to={`/@${account_name}/transfers`} activeClassName="active">
-                        {tt('g.balances')} <NotifiCounter fields="send,receive" />
-                    </Link>
-                </li>
-                <li>
-                    <Link to={`/@${account_name}/permissions`} activeClassName="active">
-                        {tt('g.permissions')} <NotifiCounter fields="account_update" />
-                    </Link>
-                </li>
-                <li>
-                    <Link to={`/@${account_name}/password`} activeClassName="active">{tt('g.password')}</Link>
-                </li>
-                <li>
-                    <Link to={`/@${account_name}/invites`} activeClassName="active">{tt('g.invites')}</Link>
-                </li>
-            </ul>
-        </div>
-        <div className="columns shrink right-column">
-            <ul className="WalletSubMenu menu">
-                <li>
-                    <Link to={`/@${account_name}/assets`} activeClassName="active">{tt('g.advanced')}</Link>
-                </li>
-            </ul>
-        </div>
-    </div>
+class WalletSubMenu extends React.Component {
+    static propTypes = {
+        account_name: PropTypes.string.isRequired
+    };
+
+    constructor() {
+        super();
+        this.showLogin = this.showLogin.bind(this)
+    }
+
+    showLogin = (e) => {
+        e.preventDefault();
+        this.props.showLogin({username: this.props.account_name, authType: 'active'});
+    };
+
+    render() {
+        const { account_name } = this.props;
+
+        return (
+            <div className="row">
+                <div className="columns small-10 medium-12 medium-expand left-column">
+                    <ul className="WalletSubMenu menu">
+                        <li>
+                            <Link to={`/@${account_name}/transfers`} activeClassName="active">
+                                {tt('g.balances')} <NotifiCounter fields="send,receive" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={`/@${account_name}/permissions`} activeClassName="active">
+                                {tt('g.permissions')} <NotifiCounter fields="account_update" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={`/@${account_name}/password`} activeClassName="active">{tt('g.password')}</Link>
+                        </li>
+                        <li>
+                            <Link to={`/@${account_name}/invites`} activeClassName="active">{tt('g.invites')}</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="columns shrink right-column">
+                    <ul className="WalletSubMenu menu">
+                        <li>
+                            <Link to={`/@${account_name}/assets`} /* onClick={this.showLogin} */ activeClassName="active" >{tt('g.advanced')}</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
 }
+
+export default connect(
+    (state, ownProps) => ownProps,
+    dispatch => ({
+        showLogin: ({username, authType}) => {
+            dispatch(user.actions.showLogin({loginDefault: {username, authType}}))
+        }
+    })
+
+)(WalletSubMenu)

a2df4cb7b0bead01096d23fbd7114d79bb8a03e5
b1acksun <tkachenko.igor88@gmail.com>
2017-08-21 22:55:23 +0300

    add getBalance

--- app/utils/MarketUtils.js
+++ app/utils/MarketUtils.js
@@ -1,4 +1,6 @@
-function roundUp(num, precision) {
+import { LIQUID_TICKER, DEBT_TICKER } from 'app/client_config'
+
+export function roundUp(num, precision) {
     let satoshis = parseFloat(num) * Math.pow(10, precision)
 
     // Attempt to correct floating point: 1.0001 satoshis should not round up.
@@ -8,7 +10,7 @@ function roundUp(num, precision) {
     return Math.ceil(satoshis) / Math.pow(10, precision)
 }
 
-function roundDown(num, precision) {
+export function roundDown(num, precision) {
     let satoshis = parseFloat(num) * Math.pow(10, precision)
 
     // Attempt to correct floating point: 1.9999 satoshis should not round down.
@@ -18,7 +20,33 @@ function roundDown(num, precision) {
     return Math.floor(satoshis) / Math.pow(10, precision)
 }
 
-module.exports = {
-    roundUp,
-    roundDown
+export function isMarketAsset(quote, base) {
+	let isMarketAsset = false, marketAsset, inverted = false;
+
+	if (quote.get("bitasset") && base.get("id") === quote.getIn(["bitasset", "options", "short_backing_asset"])) {
+		isMarketAsset = true;
+		marketAsset = {id: quote.get("id")};
+	} else if (base.get("bitasset") && quote.get("id") === base.getIn(["bitasset", "options", "short_backing_asset"])) {
+		inverted = true;
+		isMarketAsset = true;
+		marketAsset = {id: base.get("id")};
+	}
+
+	return {
+		isMarketAsset,
+		marketAsset,
+		inverted
+	};
+}
+
+export function getBalance(account, base, quote, isAsk) {
+	if (base === LIQUID_TICKER && quote === DEBT_TICKER) {
+		return isAsk
+			? account.balance
+			: account.sbd_balance
+	}
+
+	return isAsk
+		? account.balance
+		: account.assets_balance[quote]
 }

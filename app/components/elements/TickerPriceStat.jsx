c1aef24e0ba6ff287514b7af6d3e3e3dce197359
b1acksun <tkachenko.igor88@gmail.com>
2017-12-14 04:37:53 +0200

    minor changes

--- app/components/elements/TickerPriceStat.jsx
+++ app/components/elements/TickerPriceStat.jsx
@@ -1,10 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import {roundUp, roundDown} from 'app/utils/MarketUtils';
 import tt from 'counterpart';
 
 export default class TickerPriceStat extends Component {
 
+  	static propTypes = {
+		account: PropTypes.string,
+		ticker: PropTypes.object.isRequired,
+	}
+
+    constructor() {
+        super()
+        this.state = {}
+    }
+
     render() {
-        const { ticker, assetName } = this.props;
+		const { assetName } = this.props;
+		let ticker = {
+            latest:         0,
+            lowest_ask:     0,
+            highest_bid:    0,
+            percent_change: 0,
+			base_volume:    0,
+			quote_volume:   0
+        };
+
+
+        if(typeof this.props.ticker != 'undefined') {
+            let { latest, lowest_ask, highest_bid, percent_change, base_volume, quote_volume } = this.props.ticker;
+            // let { base, quote } = this.props.feed
+            ticker = {
+                latest:         parseFloat(latest),
+                lowest_ask:     roundUp(parseFloat(lowest_ask), 6),
+                highest_bid:    roundDown(parseFloat(highest_bid), 6),
+                percent_change: parseFloat(percent_change),
+				quote_volume:     (parseFloat(quote_volume)),
+                // feed_price:     parseFloat(base.split(' ')[0]) / parseFloat(quote.split(' ')[0])
+				base_volume: (parseFloat(base_volume)),
+            }
+        }
 
         const pct_change = <span className={'Market__ticker-pct-' + (ticker.percent_change < 0 ? 'down' : 'up')}>
                 {ticker.percent_change < 0 ? '' : '+'}{ticker.percent_change.toFixed(3)}%

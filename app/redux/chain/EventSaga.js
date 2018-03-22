5a4532cd214f9986e1a829fe21db0320c1f60d93
Alex Gu <sualexx@aol.com>
2018-03-22 06:54:15 +0300

    #593 fix holes, transfer notify

--- app/redux/chain/EventSaga.js
+++ app/redux/chain/EventSaga.js
@@ -1,10 +1,76 @@
-import {call} from 'redux-saga/effects';
+import {call, put, select, fork} from 'redux-saga/effects';
+
+const touched = [];
+
 
 export default function* watchMessages(msgSource) {
   console.log('------------------------- begin receive messages')
   let message = yield call(msgSource.nextMessage)
+  const currentUser = yield select(state => state.user.get('current'));
+  const username = currentUser.get('username')
+
+  console.log(`^^^^^^^^^^^^^^^^^^^^^^^^^^^`)
+  console.log(username)
+
   while (message) {
-    console.log(message)
+    //
+    let data;
+    //
+    try {
+      data = JSON.parse(message)
+    } catch (e) {
+      message = yield call(msgSource.nextMessage)
+    }
+
+    //
+    const {id, result} = data;
+    //
+    // // const currentTrxId;
+    if (id >= 2) {
+      // transactions
+      // debounce duplicates (bug!)
+      // block operations
+      if (result.length > 0) {
+        for(let trx of result) {
+          const {trx_id, op} = trx;
+          if (!touched.includes(trx_id)) {
+            touched.push(trx_id)
+          //  process operation here
+            const [type, payload] = op;
+            console.log(type);
+            console.log(payload)
+            if (type === 'transfer') {
+              const {to, from, amount} = payload;
+              if (to === username) {
+                yield put({
+                  type: 'ADD_NOTIFICATION',
+                  payload: {
+                    key: "chain_" + Date.now(),
+                    message: `User ${from} transferred you ${amount}`,
+                    dismissAfter: 3000
+                  }})
+
+
+
+              }
+            }
+
+
+
+          }
+          else {
+            console.log(`BLAAAAAAAAAAAAAAAAA`)
+            touched.splice(touched.indexOf(trx_id), 1);
+          }
+        }
+
+        console.log(`@@@@@@@@@@@@@@@@@@@@@@@@@@`)
+        console.log(touched)
+
+      }
+    }
+
+
     message = yield call(msgSource.nextMessage)
   }
   console.log('------------------------- done receive messages')
--- app/redux/chain/Tracker.js
+++ app/redux/chain/Tracker.js
@@ -1,14 +1,84 @@
 import PersistentWebSocket from 'app/utils/PersistentWebSocket'
 
-console.log(`@@@@@@@@@@@@@@@@@@@@@@@@@`)
-console.log(PersistentWebSocket)
-
-export default function tracker(url = 'wss://ws.golos.io') {
-  const source = new PersistentWebSocket(url)
+export default function tracker(url = 'ws://localhost:8090') {
+  const ws = new PersistentWebSocket(url)
   let deferred
+  //
+  const redKey = "lastblocknumber"
+  console.log(`{tracker} started`)
+  const client = localStorage
+  client.removeItem(redKey);
+  // client.setItem(redKey, 14902800);
 
-  source.onopen = event => {
-    source.send(JSON
+
+  const END = "\x1b[0m"
+  const RED = "\x1b[31m"
+  const GREEN = "\x1b[36m"
+  //
+  let height = 0
+  let next = 0
+  let nodeparam = "now"//process.argv.slice(2);
+  let getNOW = nodeparam === "now"
+  let targetheight = (!isNaN(nodeparam[0])) ? nodeparam[0] : false
+  let fheight = 0
+  let timestamp = 0
+  //
+  if (targetheight) {
+    fheight = Number(targetheight);
+    client.setItem(redKey, fheight);
+    targetheight = false;
+  }
+  //
+  const Send = (operations, ProcessedBlockNum, ProcessedOpTime) => {
+    let ops = []
+    //
+    for (let op of operations) {
+      ops.push(op.op)
+    }
+    //
+    let JSONops = JSON.stringify(ops)
+    let opslength = ops.length
+    let delta = height + 1 - ProcessedBlockNum
+    let state = (ProcessedBlockNum > height) ? "Realtime" : "🏃 rocessing missed blocks... " + delta + " Left"
+    let golostime = Date.parse(timestamp)
+    // let ageLastOps = (golostime - ProcessedOpTime) / 1000
+    //
+    // console.log(`${ProcessedBlockNum} [${height + 1}] ${state} ops count : ${ops.length}`)
+    //
+    client.setItem(redKey, ProcessedBlockNum);
+    if (ProcessedBlockNum <= height) getOps(ProcessedBlockNum + 1, 3)
+    // return pub.publish(CHANNEL, JSONops);
+
+    // if (deferred) {
+    //   deferred.resolve(ops)
+    //   deferred = null
+    // }
+
+
+
+  }
+  //
+  const getOps = (sequentBlock, speed) => {
+    ws.send(JSON.stringify({
+      id: speed,
+      method: 'call',
+      params: ["database_api", "get_ops_in_block", [sequentBlock, "false"]]
+    }), (e) => {
+      if (e) return console.warn(e)
+    });
+
+  }
+  //
+  let Tl = D => {
+    let txTimes = []
+    for (tx of D) {
+      txTimes.push(Date.parse(tx.timestamp))
+    }
+    return Math.max.apply(Math, txTimes);
+  }
+  //
+  ws.onopen = event => {
+    ws.send(JSON
       .stringify({
         id: 1,
         method: 'call',
@@ -17,9 +87,78 @@ export default function tracker(url = 'wss://ws.golos.io') {
       if (e) return console.warn(e)
     });
   }
+  //
+  ws.onmessage = e => {
 
-  source.onmessage = event => {
+    const {data: raw} = e;
+
+    // console.log(`%%%%%%%%%%%%%%%%%%%%%%%`)
+    // console.log(raw)
+
+    let data = JSON.parse(raw)
+    if (data.method === "notice" && data.params) {
+      let hex = data.params[1][0].previous.slice(0, 8)
+      height = parseInt(hex, 16)
+      timestamp = data.params[1][0].timestamp
+      // console.log(`--------------------------`)
+      // console.log(`[getNOW] ${getNOW}`)
+      // console.log(`[height] ${height}`)
+      // console.log(`[fheight] ${fheight}`)
+      // not called when started with last remembered block (no cli params)
+      if (getNOW || height < fheight) {
+        // console.log(`[set redKey] ${height}`)
+        client.setItem(redKey, height);
+      }
+      // get the last processed key from store
+      const num = client.getItem(redKey)
+      const process = (num) => {
+        // console.log(`[get redKey] ${num}`)
+        let lastblock = Number(num)
+        next = height - 1
+        // console.log(`[lastblock] ${lastblock}`)
+        // console.log(`[next] ${next}`)
+        // the last processed block is stored
+        // start counting from it
+        if (lastblock) {
+          next = lastblock + 1
+        }
+        // console.log(`[next] ${next}`)
+        let delta = height - next
+        // console.log(`[delta] ${delta}`)
+        // delta = height - next -> next = lastblock +1
+        // realtime mode
+        if (delta < 0)
+          return getOps(next, 2)
+        // catch up mode
+        else if (lastblock < height)
+          // console.log(`<<<<<<<<<<<<<<------------------------------------->>>>>>>>>>>>>>>>`)
+        return getOps(next, 3)
+      }
+      process(num)
+    }
+    //
+    else if (data.id === 2) {
+      // let lastTime = Tl(data.result)
+      Send(data.result, next, 0)
+    }
+    //
+    else if (data.id === 3) {
+      // console.log(`<<<<<<< catch up data`)
+      const num = client.getItem(redKey)
+      const process = (num) => {
+        // console.log(`<< get redKey`)
+        let lastblock = Number(num)
+        if (lastblock > height) return
+        // let lastTime = Tl(data.result)
+        // console.log(`<< [lastblock] ${lastblock}`)
+        // console.log(`<< [height] ${height}`)
+        Send(data.result, lastblock + 1, 0)
+      }
+      process(num)
+    }
+
     if (deferred) {
+      // console.log(`------------------------------`)
       deferred.resolve(event.data)
       deferred = null
     }
